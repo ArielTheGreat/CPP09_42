2be56a2893c604677a1639f53d0342ac031cdeac
frocha <frocha@student.42heilbronn.de>
2025-05-11 18:58:41 +0200

    added implementaion header

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -1,2 +1,10 @@
 #include "BitcoinExchange.hpp"
 
+BitcoinExchange::BitcoinExchange(){}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& other){}
+
+BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other) {}
+
+BitcoinExchange::~BitcoinExchange() {}
+
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -2,13 +2,31 @@
 #define BITCOINEXCHANGE_HPP
 
 #include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using valueMap = std::multimap<std::string, float>;
+using valuePair = std::pair<std::string, float>;
 
 class BitcoinExchange
 {
     private:
-        
+        valueMap _dataBaseMap;
+        valueMap _inputMap;
     public:
+        //Orthodox canonical form
+        BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange& other);
+		BitcoinExchange& operator=(const BitcoinExchange& other);
+		~BitcoinExchange();
 
+        void parseDataBase(const std::string& fileName);
+        void parseInputFile(const std::string& fileName);
+        void calculateValues();
+        void findClosest();
 };
 
 #endif
