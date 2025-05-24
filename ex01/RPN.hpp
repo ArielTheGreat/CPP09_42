5d9f01648eb6804587ee276d3e8298b2abcf8ce3
Franco Rocha <franco.rocha@fast2work.de>
2025-05-24 10:44:04 +0200

    added implementation of ex01 functions

--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -5,3 +5,24 @@
 #include <sstream>
 #include <string>
 
+using string = std::string;
+using stack = std::stack<int>;
+using array = std::array<string, 30>;
+
+class RPN {
+	private:
+		stack _stack;
+		array _input;
+	public:
+		RPN() = default;
+		RPN(const string& input);
+		RPN(const RPN& other);
+		RPN& operator=(const RPN& other);
+		~RPN();
+
+		void calculate();
+	};
+
+bool isInputValid(const array& input);
+array split(const string& str, char delimiter);
+bool isNumeric(const std::string& str);
